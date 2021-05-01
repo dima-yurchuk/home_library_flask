11aadc244e69cf67412c1576002f1f141c4c9a7d
dima-yurchuk <dimayurchuk20@gmail.com>
2021-05-01 15:14:31 +0300

    create database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super secret key'
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
 
 from app import views
